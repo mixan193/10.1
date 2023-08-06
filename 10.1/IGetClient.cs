c32e06c6efe9d6c560b8b7d8fd134277adbc0979
Mihael Kardash <mixan193@gmail.com>
2023-08-06 21:53:05 +0300

    Добавьте файлы проекта.

---
+++ 10.1/IGetClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1
+{
+    internal interface IGetClient
+    {
+        string GetClientInString(Client client);
+    }
+}
---
+++ 10.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберте роль:\r\n" +
+                "1. Консультант;");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    Consultant();
+                    break;
+                default:
+                    Console.CursorLeft = 0;
+                    Console.WriteLine("Вы ничего не выбрали");
+                    Exit();
+                    break;
+            }
+            void Exit()
+            {
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            void Consultant()
+            {
+                Consultant consultant = new Consultant();
+                consultant.Handler();
+            }
+        }
+
+    }
+}
